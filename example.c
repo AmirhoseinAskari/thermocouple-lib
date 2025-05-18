afee2af8c71f243a995ef3ff0afd043cbf8dfa85
Amirhossein Askari <theamiraskarii@gmail.com>
2025-05-18 10:59:38 +0330

    example code

--- example.c
+++ example.c
@@ -9,13 +9,15 @@ ThermocoupleType TC;
 
 void main(void)
 {
-	TC = TC_TYPE_K;
+	TC = TC_TYPE_K;    // Set thermocouple type to Type K
 	
+	// Calculate temperature from voltage (17.85 mV)
 	temperature = TC_CalculateTemperature(TC, 17.85); 
 	
 	sprintf(str, "Temperature is %f", temperature);
 	puts(str);
 	
+	// Calculate voltage from temperature (-156 °C)
 	voltage = TC_CalculateVoltage(TC, -156);
 	
 	sprintf(str, "Voltage is %f", voltage);
